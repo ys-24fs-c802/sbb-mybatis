4eece56a256107a893b19bf469c198b2f8dd4a99
shinis(ys) <actionshin@gmail.com>
2024-09-30 10:10:00 +0900

    Hello World

---
+++ src/main/java/com/mysite/sbbmybatis/SbbMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.sbbmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbMybatisApplication.class, args);
+	}
+
+}

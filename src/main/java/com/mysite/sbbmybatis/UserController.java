734bbb11122af61a3774996db7cab9ccc5b5298b
shinis(ys) <actionshin@gmail.com>
2024-10-08 16:04:09 +0900

    mapper에서 데이터를 가져와 html에 적용하기

--- src/main/java/com/mysite/sbbmybatis/UserController.java
+++ src/main/java/com/mysite/sbbmybatis/UserController.java
@@ -2,6 +2,7 @@ package com.mysite.sbbmybatis;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,10 +21,9 @@ public class UserController {
 	
 	// 상세정보 /user/hong
 	@GetMapping("/user/{username}")
-	@ResponseBody
-	public User getUser(@PathVariable("username") String username) {
-		// String username = "hong";
-		// 사용자 아이디에 맞는 정보를 가져온다. --- 서비스
-		return userService.getUser(username);
+	public String getUser(@PathVariable("username") String username, Model model) {
+		User user = userService.getUser(username);
+		model.addAttribute("user", user);
+		return "/user/detail";
 	}
 }

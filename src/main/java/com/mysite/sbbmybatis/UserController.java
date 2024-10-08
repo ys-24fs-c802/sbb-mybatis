888039c12f950b73bf6c7aff430008af5a9eadb8
shinis(ys) <actionshin@gmail.com>
2024-10-08 14:39:17 +0900

    getUser() 구현

--- src/main/java/com/mysite/sbbmybatis/UserController.java
+++ src/main/java/com/mysite/sbbmybatis/UserController.java
@@ -1,12 +1,29 @@
 package com.mysite.sbbmybatis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	// 로그인 페이지
 	@GetMapping("/user/login")
 	public String login() {
 		return "/user/login";
+	}
+	
+	// 상세정보 /user/hong
+	@GetMapping("/user/{username}")
+	@ResponseBody
+	public User getUser(@PathVariable("username") String username) {
+		// String username = "hong";
+		// 사용자 아이디에 맞는 정보를 가져온다. --- 서비스
+		return userService.getUser(username);
 	}
 }
---
+++ src/main/java/com/mysite/sbbmybatis/UserService.java
@@ -0,0 +1,21 @@
+package com.mysite.sbbmybatis;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+	
+	public User getUser(String username) {
+		User user1 = new User();
+		user1.setId(1);
+		user1.setUsername(username);
+		LocalDateTime now = LocalDateTime.now();
+		user1.setCreatedAt(now);
+		user1.setUpdatedAt(now);
+		
+		return user1;
+	}
+
+}
